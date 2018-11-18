a16e7a850a93186d1036ed14f981f0e12d3d1b96
parth <srivastavaparth2016@gmail.com>
2018-11-18 15:57:18 +0530

    initial commit

---
+++ app/app.style.js
@@ -0,0 +1,28 @@
+export default{
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center'
+  },
+  input: {
+    textAlign: 'center',
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginTop: 30,
+    minWidth: 150
+  },
+  button: {
+    marginTop: 30,
+    minWidth: 150,
+    color: 'blue'
+  }
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import App from './app/app';
+import {name as appName} from './app.json';
+import {AppRegistry} from 'react-native';
+
+AppRegistry.registerComponent(appName, () => App);
